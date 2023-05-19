5f561d3520964e7a3ee099314a80519d4d20bd8c
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-05-19 19:40:11 +0900

    add use fmt

--- src/tips/use-fmt.go
+++ src/tips/use-fmt.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+type F struct {
+	Name string
+	Age  int
+	Gs   []*G
+}
+
+func (f *F) String() string {
+	return fmt.Sprintf("NAME=%q, AGE=%d, G=%v", f.Name, f.Age, f.Gs)
+}
+
+type G struct {
+	Type string
+}
+
+func (g *G) String() string {
+	return fmt.Sprintf("Type=%q", g.Type)
+}
+
 func main() {
-	$END$
+	f := &F{
+		Name: "John",
+		Age:  20,
+		Gs: []*G{
+			&G{
+				Type: "A-type",
+			},
+			&G{
+				Type: "B-type",
+			},
+		},
+	}
+
+	fmt.Printf("%v\n", f)
+	fmt.Printf("%#v\n", f)
+	fmt.Printf("%T\n", f)
 }
