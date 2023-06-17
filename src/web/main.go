24805257318595d758ae5c760ed982f7e6775972
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-06-17 20:39:33 +0900

    move func value

--- src/web/main.go
+++ src/web/main.go
@@ -5,15 +5,17 @@ import (
 	"net/http"
 )
 
+func myHandler(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case http.MethodGet:
+		fmt.Fprintf(writer, "Get:  Hello, World!")
+	case http.MethodPost:
+		fmt.Fprintf(writer, "Post: Hello, World!")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		switch request.Method {
-		case http.MethodGet:
-			fmt.Fprintf(writer, "Get:  Hello, World!")
-		case http.MethodPost:
-			fmt.Fprintf(writer, "Post: Hello, World!")
-		}
-	})
+	http.HandleFunc("/", myHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
