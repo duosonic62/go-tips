2b1119b0401ca64d708cac116b83422aec16b074
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-09-13 20:59:19 +0900

    add chan fanIn

--- src/concurrent/generator.go
+++ src/concurrent/generator.go
@@ -5,6 +5,23 @@ import (
 	"time"
 )
 
+func fanIn(ch1, ch2 <-chan string) <-chan string {
+	new_ch := make(chan string)
+	go func() {
+		for {
+			new_ch <- <-ch1
+		}
+	}()
+
+	go func() {
+		for {
+			new_ch <- <-ch2
+		}
+	}()
+
+	return new_ch
+}
+
 func generator(msg string) <-chan string {
 	ch := make(chan string)
 	go func() {
@@ -17,7 +34,7 @@ func generator(msg string) <-chan string {
 	return ch
 }
 func main() {
-	ch := generator("Hello")
+	ch := fanIn(generator("Hello"), generator("Bye"))
 	for i := 0; i < 10; i++ {
 		fmt.Println(<-ch)
 	}
