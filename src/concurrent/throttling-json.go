d2a6ea1404b881d1af9638e3d68969f1b31de8bc
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-09-23 11:39:25 +0900

    add sinple goroutine

--- src/concurrent/throttling-json.go
+++ src/concurrent/throttling-json.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+func downloadJSON(u string) {
+	// download
+	println(u)
+	time.Sleep(time.Second * 6)
+}
+
+func main() {
+	before := time.Now()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+
+			u := fmt.Sprintf("http://example.com/api/users?id=%d", i)
+			downloadJSON(u)
+		}()
+	}
+	wg.Wait()
+
+	fmt.Printf("%v\n", time.Since(before))
 }
