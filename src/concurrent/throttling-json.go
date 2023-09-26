6894e9e7ed711528625e3983d9cb2d1194eac4cf
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-09-26 20:31:34 +0900

    add simple test

--- src/concurrent/throttling-json.go
+++ src/concurrent/throttling-json.go
@@ -15,15 +15,22 @@ func downloadJSON(u string) {
 func main() {
 	before := time.Now()
 
+	// 並行数が20までに制限する
+	limit := make(chan struct{}, 20)
 	var wg sync.WaitGroup
 	for i := 0; i < 100; i++ {
 		wg.Add(1)
 		i := i
 		go func() {
+			// limitに追加する
+			limit <- struct{}{}
 			defer wg.Done()
 
 			u := fmt.Sprintf("http://example.com/api/users?id=%d", i)
 			downloadJSON(u)
+
+			// limitから取り出す
+			<-limit
 		}()
 	}
 	wg.Wait()
