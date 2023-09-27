afb48f0e6ab9d27569c5b191f81f39bce70f3429
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-09-27 20:09:02 +0900

    add table driven tests

--- src/hsd/distance_test.go
+++ src/hsd/distance_test.go
@@ -9,3 +9,25 @@ func TestStringDistance(t *testing.T) {
 		t.Fatalf("expected: %v, got: %v", want, got)
 	}
 }
+
+func TestStringDistance_TableDriven(t *testing.T) {
+	tests := []struct {
+		name string
+		lhs  string
+		rhs  string
+		want int
+	}{
+		{name: "lhs is longer than rhs", lhs: "foo", rhs: "fo", want: -1},
+		{name: "lhs is shorter than rhs", lhs: "fo", rhs: "foo", want: -1},
+		{name: "no diff", lhs: "foo", rhs: "foo", want: 0},
+		{name: "3 diffs", lhs: "foo", rhs: "hhh", want: 3},
+		{name: "multibyte", lhs: "あいう", rhs: "あいえ", want: 1},
+	}
+
+	for _, tc := range tests {
+		got := StringDistance(tc.lhs, tc.rhs)
+		if got != tc.want {
+			t.Fatalf("%s expected: %v, got: %v", tc.name, tc.want, got)
+		}
+	}
+}
