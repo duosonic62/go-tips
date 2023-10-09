1b443f35fe64ce352b8f0ae2bd0d9b4f4f6ba4dd
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-10-09 11:06:48 +0900

    add dataframego bench tests

--- src/benchmark/df_test.go
+++ src/benchmark/df_test.go
@@ -2,7 +2,10 @@ package benchmark
 
 import (
 	"bytes"
+	"context"
 	"github.com/go-gota/gota/dataframe"
+	"github.com/rocketlaunchr/dataframe-go/exports"
+	"github.com/rocketlaunchr/dataframe-go/imports"
 	"github.com/tobgu/qframe"
 	"os"
 	"testing"
@@ -38,6 +41,29 @@ func BenchmarkGota(b *testing.B) {
 		df := dataframe.ReadCSV(bytes.NewReader(bs))
 		df = df.Select([]string{"sepal_length", "species"})
 		err = df.WriteCSV(&out, dataframe.WriteHeader(true))
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkDataframeGo(b *testing.B) {
+	bs, err := os.ReadFile("iris.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var out bytes.Buffer
+		df, err := imports.LoadFromCSV(context.Background(), bytes.NewReader(bs))
+		if err != nil {
+			b.Fatal(err)
+		}
+		df.RemoveSeries("sepal_width")
+		df.RemoveSeries("petal_length")
+		df.RemoveSeries("petal_width")
+		err = exports.ExportToCSV(context.Background(), &out, df)
 		if err != nil {
 			b.Fatal(err)
 		}
