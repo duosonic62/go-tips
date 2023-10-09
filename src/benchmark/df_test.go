7111f26e2796ab552b2f254e69eee4eb7e0bd856
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-10-09 11:02:20 +0900

    add gota bench tests

--- src/benchmark/df_test.go
+++ src/benchmark/df_test.go
@@ -2,6 +2,7 @@ package benchmark
 
 import (
 	"bytes"
+	"github.com/go-gota/gota/dataframe"
 	"github.com/tobgu/qframe"
 	"os"
 	"testing"
@@ -19,6 +20,24 @@ func BenchmarkQFrame(b *testing.B) {
 		qf := qframe.ReadCSV(bytes.NewReader(bs))
 		qf = qf.Select("sepal_length", "species")
 		err = qf.ToCSV(&out)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkGota(b *testing.B) {
+	bs, err := os.ReadFile("iris.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var out bytes.Buffer
+		df := dataframe.ReadCSV(bytes.NewReader(bs))
+		df = df.Select([]string{"sepal_length", "species"})
+		err = df.WriteCSV(&out, dataframe.WriteHeader(true))
 		if err != nil {
 			b.Fatal(err)
 		}
