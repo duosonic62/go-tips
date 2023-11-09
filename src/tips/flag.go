5dd707102ba76f5897f6e6c1b7b84eeae16ee1eb
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-11-09 21:31:15 +0900

    add flag sample

--- src/tips/flag.go
+++ src/tips/flag.go
@@ -6,8 +6,13 @@ func main() {
 	// return pointer
 	max := flag.Int("max", 255, "max value")
 	name := flag.String("name", "something", "my name")
+
+	var min int
+	var id string
+	flag.IntVar(&min, "min", 0, "min value")
+	flag.StringVar(&id, "id", "id", "your account id")
+
 	flag.Parse()
 
-	println(*name, *max)
-
+	println(*name, *max, min, id)
 }
